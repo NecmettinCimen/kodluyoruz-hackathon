f636c5ccd175d212b0fb6939b222c2f6b0f60d27
Necmettin <mail@necmettin.me>
2020-12-26 16:07:22 +0300

    publish

---
+++ precache-manifest.8cf9abd96a5f2ba26017741f543010b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa418f9f437599c79055a0da625a3a97",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56d9f5e4039b202115af",
+    "url": "/static/css/2.cf29e716.chunk.css"
+  },
+  {
+    "revision": "a7601c4fe812ae1102c6",
+    "url": "/static/css/main.5a83e679.chunk.css"
+  },
+  {
+    "revision": "56d9f5e4039b202115af",
+    "url": "/static/js/2.c9ee0e6e.chunk.js"
+  },
+  {
+    "revision": "f0c1093415b06c9dc10783dbb242bbcc",
+    "url": "/static/js/2.c9ee0e6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7601c4fe812ae1102c6",
+    "url": "/static/js/main.d00a517a.chunk.js"
+  },
+  {
+    "revision": "03f52ba83c993d62c98a",
+    "url": "/static/js/runtime-main.cbb77c7a.js"
+  }
+]);
