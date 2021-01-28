1d4cd8549a6b4cb68b6fab669633094030fdbdf8
Necmettin <mail@necmettin.me>
2021-01-28 18:01:35 +0300

    Updates

---
+++ precache-manifest.119e44df0a460683447a8820233ecf74.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae1f87d4969b1384fc853fb0b50a0503",
+    "url": "/kodluyoruzhackathon/index.html"
+  },
+  {
+    "revision": "060598802787412210bc",
+    "url": "/kodluyoruzhackathon/static/css/2.cf29e716.chunk.css"
+  },
+  {
+    "revision": "c3ee12d2ea917a5a88db",
+    "url": "/kodluyoruzhackathon/static/css/main.62ccda5d.chunk.css"
+  },
+  {
+    "revision": "060598802787412210bc",
+    "url": "/kodluyoruzhackathon/static/js/2.1daaafba.chunk.js"
+  },
+  {
+    "revision": "f0c1093415b06c9dc10783dbb242bbcc",
+    "url": "/kodluyoruzhackathon/static/js/2.1daaafba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3ee12d2ea917a5a88db",
+    "url": "/kodluyoruzhackathon/static/js/main.825be73e.chunk.js"
+  },
+  {
+    "revision": "6b01a0291a8d64deedd6",
+    "url": "/kodluyoruzhackathon/static/js/runtime-main.8e55bad5.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.19fed7c2abaa2173cc94539d37763158.js"
+  "/kodluyoruzhackathon/precache-manifest.119e44df0a460683447a8820233ecf74.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/kodluyoruzhackathon/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
